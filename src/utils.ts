cd12a7e7084d3628d494549e8e0af64694fc3af4
DGCK81LNN <54282183+DGCK81LNN@users.noreply.github.com>
2024-02-16 16:12:29 +0800

    fix: fix tryRestoreRawText

--- src/utils.ts
+++ src/utils.ts
@@ -18,14 +18,17 @@ export const ahoFixes: Record<string, string[]> = {
  * without escaping literal special characters. This function tries to get the
  * raw text input from `argv.source`.
  */
-export function tryRestoreRawText(text: string, source: string) {
+export function tryRestoreRawText(text: string, source: string, strip = false) {
   const unescapedSource = h.unescape(source)
   const start = unescapedSource.lastIndexOf(text)
-  if (start !== -1) return null
+  if (start === -1) return null
   const unescapedBefore = unescapedSource.slice(0, start)
   for (let i = 1; i < source.length; i++) {
     const head = h.unescape(source.slice(0, i))
-    if (head === unescapedBefore) return source.slice(i)
+    if (head === unescapedBefore) {
+      const raw = source.slice(i)
+      if (strip) return stripTags(raw)
+    }
   }
   return null
 }
